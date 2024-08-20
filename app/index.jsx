4f60928fda79f01b92c113ad473657a7666deabe
El Parche Dev <elparcheti@gmail.com>
2024-08-20 16:46:25 -0500

    actualización hasta 38:53

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center 
       bg-white">
-      <Text className="text-3xl">Aora ADSO!</Text>
+      <Text className="text-3xl font-pblack">Aora ADSO!</Text>
       <StatusBar style='auto' />
       <Link href="/profile" style={{ color: 'blue'}}>Go to Profile</Link>
     </View>
